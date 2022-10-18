f1a354ae04e90e6ab95540f34cb24adff2a67f60
baischsoares <baischsoares@gmail.com>
2022-10-18 11:34:51 -0300

    Criação do menu e menu mobile com HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnMobile = document.getElementById('BtnMobile')
+const btnFechar = document.getElementById('fecharMenu')
+const menuMobile = document.querySelector('.menuPaginasMobile')
+
+function openMenu(){
+menuMobile.classList.add('ativo')
+}
+
+function closeMenu(){
+  menuMobile.classList.remove('ativo')
+  }
+
+btnMobile.addEventListener('click', openMenu)
+btnFechar.addEventListener('click', closeMenu)
